8dfe3bc8b35211f77ddba1b66a0a6d411817dc06
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-07-04 16:03:06 +0200

    Updated prototype

--- custom_tests/test/test.c
+++ custom_tests/test/test.c
@@ -6,39 +6,29 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 12:44:22 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/07/02 15:38:20 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/07/04 16:02:54 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 5
 #endif
 
-// int	main(int fd)
-// {
-// 	int		c;
-// 	char	buff[BUFFER_SIZE + 1];
-// 	c = 1;
-// 	c = read(fd, &buff, BUFFER_SIZE);
-// 	if (c < 0)
-// 	{
-// 		return (0);
-// 	}
-// 	printf("%s", buff);
-// 	return(1);
-// }
-
 int	test(void)
 
 {
-	int s;
-	char *buf;
-	buf = (char *) calloc(8, sizeof(char));
-	s = read(1, buf, 8);
-	buf[s] = '\0';
-	printf("\n%s", buf);
+	int		fd;
+	char	*buff;
+	int		l_read;
+
+	buff = calloc(BUFFER_SIZE, sizeof(char));
+	fd = open("test.txt", O_RDONLY);
+	l_read = read(fd, buff, BUFFER_SIZE + 1);
+	buff[l_read +1] = '\0';
+	printf("The buffer is :%s", buff);
 	return (0);
 }
