e9b6c943b1cfe9e153fe2d1ea56a775b1d2447c8
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-07-04 17:10:26 +0200

    Expanded tester

--- custom_tests/main.c
+++ custom_tests/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/02 12:44:22 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/07/04 16:52:48 by slaszlo-         ###   ########.fr       */
+/*   Created: 2022/07/04 16:45:24 by slaszlo-          #+#    #+#             */
+/*   Updated: 2022/07/04 16:56:51 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 5
-#endif
-
-int	test(void)
+#include "../get_next_line.h"
+int main (void)
 
 {
 	int		fd;
 	char	*buff;
 	int		l_read;
-
-	buff = calloc(BUFFER_SIZE, sizeof(char));
+	
+	buff = calloc(10, sizeof(char));
 	fd = open("test.txt", O_RDONLY);
-	l_read = read(fd, buff, BUFFER_SIZE + 1);
-	buff[l_read +1] = '\0';
-	printf("The amount of stuff read by read is: %d\n", l_read);
-	printf("The buffer is :%s", buff);
-	return (0);
+	l_read = read(fd, buff, 20);
+	get_next_line(fd);
+	return(0);
 }
