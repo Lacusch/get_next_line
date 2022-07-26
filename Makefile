bd52bbb2361df9062dcd3a58587684e6a501981a
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-07-26 09:01:29 +0200

    Small fixes

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
-all: clean
+all:
 	gcc get_next_line.c get_next_line_utils.c main.c -fsanitize=address
 norm:
 	norminette get_next_line.c get_next_line_utils.c main.c
 clean:
 	rm -f a.out
+re: clean all
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:39:43 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/07/23 01:43:42 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/07/26 07:49:12 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@
 # include "limits.h"
 # include <fcntl.h>
 
+char	*get_next_line(int fd);
+
 char	*read_than_stash(int fd, char* stash);
 char	*cut_from_static(char* stash);
-int	get_lenght(char *stash);
-int	is_new_line (char * str);
+int		get_lenght(char *stash);
+int		is_new_line (char * str);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
 char	*move_str(char *dest, char *src, size_t len);
 char	*ft_strjoin(char *s1, char *s2);
-int	ft_strchr_bool(const char *s, int c);
-char	*get_next_line(int fd);
+int		ft_strchr_bool(const char *s, int c);
